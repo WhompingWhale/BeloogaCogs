2287491fb4d086dcf21e9f4a455b9057fd32a531
WhompingWhale <whompingwhale@gmail.com>
2017-02-13 13:30:07 -0800

    Create beemovie.py

---
+++ beemovie/beemovie.py
@@ -0,0 +1,46 @@
+﻿import discord
+from discord.ext import commands
+
+class Mycog:
+    """According to all known laws of aviation..."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mycom(self):
+        """Bees Bees Bees"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+        await self.bot.say("I can do stuff!")
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
